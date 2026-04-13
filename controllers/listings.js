51e047b92a7b0654defee3a2677ce4904d5f72a5
1Aayush1130 <aayusingrath@gmail.com>
2026-04-13 18:23:12 +0530

    Add Project Files

---
+++ controllers/listings.js
@@ -0,0 +1,91 @@
+const Listing = require("../models/listing");
+
+module.exports.index = async (req , res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs" , {allListings});
+};
+
+module.exports.renderNewForm = (req, res) => {
+    res.render("listings/new.ejs");
+};
+
+module.exports.showListing = async (req , res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id)
+        .populate({
+            path: "reviews" , 
+            populate: {
+                path: "author" ,
+            }, 
+        })
+        .populate("owner");
+    if(!listing) {
+        req.flash("error" , "Listing you are requested for does not exist!");
+        return res.redirect("/listings");
+    }
+
+    console.log(listing);
+    res.render("listings/show.ejs" , { listing });
+};
+
+module.exports.createListing = async(req , res , next ) => {
+    let url = req.file.path;
+    let filename = req.file.filename;
+
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    newListing.image = { url , filename };
+    await newListing.save();
+    req.flash("success" , "New Listing Created");
+    res.redirect("/listings");
+};
+
+module.exports.renderEditForm = async (req , res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+
+    if(!listing) {
+        req.flash("error" , "Listing you are requested for does not exist!");
+        res.redirect("/listings");
+    }
+
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload" , "/upload/w_250");
+    res.render("listings/edit.ejs" , { listing , originalImageUrl });
+};
+
+module.exports.updateListing = async (req, res) => {
+    let {id} = req.params;
+    let listing = await Listing.findById(id);
+    
+    listing.title = req.body.listing.title;
+    listing.description = req.body.listing.description;
+    listing.price = req.body.listing.price;
+    listing.location = req.body.listing.location;
+    listing.country = req.body.listing.country;
+    
+    // Sirf tab image update karo jab user ne kuch likha ho
+    if(req.body.listing.image && req.body.listing.image.url) {
+        listing.image.url = req.body.listing.image.url;
+    }
+
+    let listingg = await Listing.findByIdAndUpdate(id , { ...req.body.listing});
+
+    if(typeof req.file !== "undefined") {
+       let url = req.file.path;
+       let filename = req.file.filename;
+       listingg.image = { url , filename };
+       await listingg.save();
+    }
+
+    req.flash("success" , "Listing Updated");
+    res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyListing = async (req , res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    req.flash("success" , "Listing Deleted");
+    res.redirect("/listings");
+};
